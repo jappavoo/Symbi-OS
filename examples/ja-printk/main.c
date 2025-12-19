900370907be02a359f0ea1fa65a0586f218b29b1
Jonathan Appavoo <jappavoo@bu.edu>
2025-12-19 10:54:34 -0500

    check point after vm crash -- git corruption

--- examples/ja-printk/main.c
+++ examples/ja-printk/main.c
@@ -2,9 +2,27 @@
 #include <string.h>
 #include "L0/sym_lib.h"
 #include "extension.h"
+#include <dlfcn.h>
+#include <assert.h>
+#include <inttypes.h>
 
 //native kernel function that will be resolved at load time
 extern int _printk(const char *fmt, ...);
+
+static inline uintptr_t get_exc_page_fault_addr()
+{
+  uintptr_t val;
+
+  val = (uintptr_t)dlsym(RTLD_DEFAULT, "asm_exc_page_fault");
+#if 0
+  __asm__ volatile (
+		    "mov exc_page_fault, %0"
+		    : "=r" (val)
+		    :
+		    : "memory" );
+#endif
+  return val;
+}
 
 int main() {
     volatile void * _printk_ptr;
@@ -12,6 +30,7 @@ int main() {
 
     printf("main: calling elevate: %p \n", _printk_ptr);
 
+    
     sym_elevate();
     _printk("hello world\n");
     printf("main: called elevate\n");
@@ -24,7 +43,15 @@ int main() {
     int pid = current_pid();
     printf("printf: current_pid() = %d\n", pid);
     _printk("printk: current_pid() = %d\n", pid);
+
+#if 1
+    // run pf_hdlr_addr()
+    uintptr_t  pf = (uintptr_t) pf_hdlr_addr();
+    printf("pf: %" PRIxPTR "\n", pf);
+#endif
     sym_lower();
+
+    assert(pf == get_exc_page_fault_addr());
     
     printf("DONE\n");
     return 0;
